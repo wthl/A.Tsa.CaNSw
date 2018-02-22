e7f90a6cf0cc809590fa1f0bf776dd5b0a964e3e
wthl <wthl3000@gmail.com>
2018-02-23 02:36:53 +0300

    add basic sinatra app

---
+++ app.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:cakesnsweets.db"
+
+get '/' do
+	erb :index
+end
---
+++ public/js/app.js
@@ -0,0 +1,59 @@
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key);
+	x = x*1 + 1;
+	window.localStorage.setItem(key, x);
+	
+	update_orders_input();
+	update_orders_button();
+
+}
+
+function update_orders_input()
+{
+		var orders = cart_get_orders();
+		$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+		var text = 'Cart (' + cart_get_number_of_items() + ')';
+		$('#orders_button').val(text);
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value*1;
+		}
+	}
+	return cnt;
+}
+
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
+}
+
+
