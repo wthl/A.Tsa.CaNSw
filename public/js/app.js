3ee894eab6fe7cc60f79bb8035842969b54e1856
wthl <wthl3000@gmail.com>
2018-02-28 05:15:16 +0300

    Finish header part

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,42 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 })
 
+// Прилипающий блок
+var StickyElement = function(node){
+  var doc = $(document), 
+      fixed = false,
+      anchor = node.find('.sticky-anchor'),
+      content = node.find('.sticky-content');
+  
+  var onScroll = function(e){
+    var docTop = doc.scrollTop(),
+        anchorTop = anchor.offset().top;
+    
+    console.log('scroll', docTop, anchorTop);
+    if(docTop > anchorTop){
+      if(!fixed){
+        anchor.height(content.outerHeight());
+        content.addClass('fixed');        
+        fixed = true;
+      }
+    }  else   {
+      if(fixed){
+        anchor.height(0);
+        content.removeClass('fixed'); 
+        fixed = false;
+      }
+    }
+  };
+  
+  $(window).on('scroll', onScroll);
+};
+
+var demo = new StickyElement($('#sticky'));
+// Конец прилипающего блока
+
+
+
+
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
