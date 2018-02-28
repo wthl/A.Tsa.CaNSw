0b52596e4db7975e8d214717160a51d2e3fd9f67
wthl <wthl3000@gmail.com>
2018-02-28 17:54:34 +0300

    English version added

--- app.rb
+++ app.rb
@@ -7,5 +7,11 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:cakesnsweets.db"
 
 get '/' do
+	@default_language = "rus"
 	erb :index
+end
+
+get '/eng/' do
+	@default_language = "eng"
+	erb :index_eng
 end
