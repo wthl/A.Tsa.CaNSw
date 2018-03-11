76608872c49cc9042fbf804e429978e9bb1585ee
wthl <wthl3000@gmail.com>
2018-03-11 20:21:02 +0300

    Add basic sections stabs.

--- app.rb
+++ app.rb
@@ -6,12 +6,56 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:cakesnsweets.db"
 
+
+
 get '/' do
 	@default_language = "rus"
 	erb :index
 end
 
+get '/blog' do
+	@default_language = "rus"
+	erb :blog
+end
+
+get '/gallery' do
+	@default_language = "rus"
+	erb :gallery
+end
+
+get '/q&a' do
+	@default_language = "rus"
+	erb :qna
+end
+
+get '/contacts' do
+	@default_language = "rus"
+	erb :contacts
+end
+
+# ENGLISH VERSION
+
 get '/eng/' do
 	@default_language = "eng"
 	erb :index_eng
+end
+
+get '/eng/blog' do
+	@default_language = "eng"
+	erb :blog_eng
+end
+
+get '/eng/gallery' do
+	@default_language = "eng"
+	erb :gallery_eng
+end
+
+get '/eng/q&a' do
+	@default_language = "eng"
+	erb :qna_eng
+end
+
+get '/eng/contacts' do
+	@default_language = "eng"
+	erb :contacts_eng
 end
