c0d45020c07f6dc371b6a8f53c9a5b4213bec567
wthl <wthl3000@gmail.com>
2018-03-30 23:06:34 +0300

    Standart modal window stab added

--- public/js/app.js
+++ public/js/app.js
@@ -37,13 +37,13 @@ var demo = new StickyElement($('#sticky'));
 // Конец прилипающего блока
 
 // Плавный скролл по якорям
-$(function(){
-	$('a[href^="#"]').click(function(){
-		var target = $(this).attr('href');
-		$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
-		return false;
-	});
-});
+// $(function(){
+// 	$('a[href^="#"]').click(function(){
+// 		var target = $(this).attr('href');
+// 		$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+// 		return false;
+// 	});
+// });
 // Конец плавного скролла
 
 //---------------------------------------------------------------------------------
